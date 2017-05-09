9c4389daae9221f7690cbcf785dace732ebdccb3
insta-code1 <eoinkane1@gmail.com>
2017-05-09 12:33:03 +0100

    Tabs Added

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { FavoritesPage } from './../pages/favorites/favorites';
+import { TabsPage } from "../pages/tabs/tabs";
+import { SettingsPage } from './../pages/settings/settings';
+import { QuotesPage } from './../pages/quotes/quotes';
+import { QuotePage } from './../pages/quote/quote';
+import { LibraryPage } from './../pages/library/library';
+
+
+@NgModule({
+  declarations: [
+    MyApp,
+    TabsPage,
+    FavoritesPage,
+    LibraryPage,
+    QuotePage,
+    QuotesPage,
+    SettingsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    TabsPage,    
+    FavoritesPage,
+    LibraryPage,
+    QuotePage,
+    QuotesPage,
+    SettingsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
