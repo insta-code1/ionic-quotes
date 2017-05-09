b8cf0855108ec5e41bdf90f81cc7d5c18f4ee0d7
insta-code1 <eoinkane1@gmail.com>
2017-05-09 15:10:17 +0100

    Added on AddToFavorites() button in each quote listing

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavParams } from 'ionic-angular';
+import { NavParams, AlertController } from 'ionic-angular';
 
 import { Quote, QuoteCollection } from './../../data/quoteCollection.interface';
 
@@ -11,7 +11,9 @@ import { Quote, QuoteCollection } from './../../data/quoteCollection.interface';
 export class QuotesPage implements OnInit {
   quoteGroup: QuoteCollection;
 
-  constructor(private navParams: NavParams) { }
+  constructor(
+    private navParams: NavParams,
+    private alertCtrl: AlertController) { }
 
   // ionViewDidLoad() {
   //   this.quoteGroup = this.navParams.get('quotes');
@@ -21,5 +23,30 @@ export class QuotesPage implements OnInit {
 
   ngOnInit() {
     this.quoteGroup = this.navParams.data;
+  }
+
+  onAddToFavorite(selectedQuote: Quote){
+    const alert = this.alertCtrl.create({
+      title: 'Add Quote',
+      subTitle: 'Are you sure?',
+      message: 'Are you sure you want to add the quote?',
+      buttons: [
+        {
+          text: 'Yes, go ahead',
+          handler: () => {
+            console.log('Ok');
+          }
+        },
+        {
+          text: 'No I Changed my mind',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelled');
+          }
+        }
+      ]
+    });
+
+    alert.present();
   }
 }
